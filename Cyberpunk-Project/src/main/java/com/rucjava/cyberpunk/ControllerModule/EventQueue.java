4e93113dfd61a7887d0a26cbef00fcb85107af0e
ywt <2021201613@ruc.edu.cn>
2023-06-21 23:06:27 +0800

    update code

--- Cyberpunk-Project/src/main/java/com/rucjava/cyberpunk/ControllerModule/EventQueue.java
+++ Cyberpunk-Project/src/main/java/com/rucjava/cyberpunk/ControllerModule/EventQueue.java
@@ -1,4 +1,33 @@
 package com.rucjava.cyberpunk.ControllerModule;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+
+/** 简单封装一下 ArrayList 得到的任务队列
+ */
 public class EventQueue {
+    private ArrayList<MyEvent> queue;
+
+    public EventQueue() {
+        this.queue = new ArrayList<>();
+    }
+
+    public void addEvent(MyEvent event) {
+        this.queue.add(event);
+    }
+
+    public MyEvent popEvent() {
+        MyEvent frontEvent = this.queue.get(0);
+        this.queue.remove(0);
+        return frontEvent;
+    }
+
+    public int getEventNumber() {
+        return this.queue.size();
+    }
+
+    public boolean isEmpty() {
+        return this.queue.isEmpty();
+    }
 }
--- Cyberpunk-Project/src/main/java/com/rucjava/cyberpunk/ControllerModule/MainController.java
+++ Cyberpunk-Project/src/main/java/com/rucjava/cyberpunk/ControllerModule/MainController.java
@@ -1,5 +1,23 @@
 package com.rucjava.cyberpunk.ControllerModule;
 
+import com.rucjava.cyberpunk.DevelopUtils.LevelType;
+import com.rucjava.cyberpunk.DevelopUtils.Logger;
+
 public class MainController {
-    
+    EventQueue eventQueue;
+
+    public MainController() {
+        this.eventQueue = new EventQueue();
+    }
+
+    /** 每次调用 handleAllEvent 函数将会遍历并清空事件队列
+     */
+    public void handleAllEvent() {
+        int initEventNumber = eventQueue.getEventNumber();
+        while (!this.eventQueue.isEmpty()) {
+            int eventID = initEventNumber - eventQueue.getEventNumber();
+            Logger.log(this.getClass().getName(), "handle event " + eventID, LevelType.COMMON);
+
+        }
+    }
 }
